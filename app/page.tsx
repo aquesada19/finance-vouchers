a4b50b55ce0cf53c866b1916ff07b13ce7ae88bc
Andres Quesada <andres.qa19@gmail.com>
2026-02-01 01:27:38 -0600

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from 'next/navigation'
 import { getServerSession } from 'next-auth/next'
 import { authOptions } from '@/lib/auth'
-import DashboardPage from './(app)/page'
+import DashboardPage from './(app)/DashboardClient'
 
 export default async function Page() {
   const session = await getServerSession(authOptions)
